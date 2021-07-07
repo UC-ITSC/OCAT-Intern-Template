e0b2358b398615e7fb65e040708517a6f88ce303
Wesley Reed <reedws@ucmail.uc.edu>
2021-07-07 17:25:14 -0400

    fix api config

--- API/src/utils/Config.js
+++ API/src/utils/Config.js
@@ -1,27 +1,25 @@
 const fs = require(`fs`);
 const appRoot = require(`app-root-path`);
 
-const CONFIG_FILE_PATH = `${appRoot}/config.json`;
-let config = {};
-let server = {};
+try {
+  const CONFIG_FILE_PATH = `${appRoot}/config.json`;
 
-if (fs.existsSync(CONFIG_FILE_PATH)) {
-  config = JSON.parse(fs.readFileSync(CONFIG_FILE_PATH, `utf8`));
+  const config = JSON.parse(fs.readFileSync(CONFIG_FILE_PATH, `utf8`));
 
-  ({ server } = config);
+  module.exports = config
+} catch {
+  module.exports = {
+    database: {
+      dialect: process.env.DATABASE_DIALECT || `pg`,
+      host: process.env.DATABASE_HOST || `localhost`,
+      minconnections: process.env.DATABASE_MINCONNECTIONS || 2,
+      name: process.env.DATABASE_NAME || `risk`,
+      password: process.env.DATABASE_PASSWORD || `paul`,
+      port: process.env.DATABASE_PORT || 5432,
+      username: process.env.DATABASE_USERNAME || `postgres`,
+    },
+    server: {
+      port: server.port || 3000,
+    },
+  };
 }
-
-module.exports = {
-  database: {
-    dialect: process.env.DATABASE_DIALECT || `pg`,
-    host: process.env.DATABASE_HOST || `localhost`,
-    minconnections: process.env.DATABASE_MINCONNECTIONS || 2,
-    name: process.env.DATABASE_NAME || `risk`,
-    password: process.env.DATABASE_PASSWORD || `paul`,
-    port: process.env.DATABASE_PORT || 5432,
-    username: process.env.DATABASE_USERNAME || `postgres`,
-  },
-  server: {
-    port: server.port || 3000,
-  },
-};
