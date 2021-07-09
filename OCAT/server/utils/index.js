76b3f66c58a6d62e2477b00ad7f655cd0282b716
Wesley Reed <reedws@ucmail.uc.edu>
2021-07-09 11:46:15 -0400

    add missing utils export

--- OCAT/server/utils/index.js
+++ OCAT/server/utils/index.js
@@ -1,11 +1,13 @@
-const config = require(`./Config`);
 const ErrorHandler = require(`./ErrorHandler`);
 const IndexRoute = require(`./IndexRoute`);
 const RouteLoader = require(`./RouteLoader`);
+const client = require(`./client`);
+const config = require(`./Config`);
 
 module.exports = {
   ErrorHandler,
   IndexRoute,
   RouteLoader,
+  client,
   config,
 };
