a66f99f2b15e071fbf7b13a07eb59fd3610173f9
Wesley Reed <reedws@ucmail.uc.edu>
2021-07-09 11:45:07 -0400

    fix circular dependency

--- OCAT/server/utils/client.js
+++ OCAT/server/utils/client.js
@@ -1,5 +1,5 @@
 const { createJsonClient } = require(`restify-clients`);
-const { config } = require(`../utils`);
+const { config } = require(`./Config`);
 
 module.exports = createJsonClient({
   url: config.api.url,
