e1c3c69e2e21e3997a2571bc7e7c82a01a533f51
Paul Wilson <54870992+wilsonpg@users.noreply.github.com>
2021-07-14 15:35:20 -0400

    Update client.js

--- OCAT/server/utils/client.js
+++ OCAT/server/utils/client.js
@@ -1,5 +1,5 @@
 const { createJsonClient } = require(`restify-clients`);
-const { config } = require(`./Config`);
+const config = require(`./Config`);
 
 module.exports = createJsonClient({
   url: config.api.url,
