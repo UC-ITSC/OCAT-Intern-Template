78b627391a3b64bcd7fb163c6d1eb23f45fa3459
Wes Reed <reedws@ucmail.uc.edu>
2022-12-15 12:38:58 -0500

    fix the nav bar

--- packages/client/src/components/Navigation.jsx
+++ packages/client/src/components/Navigation.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export const Navigation = () =>
   <header>
     <Navbar expand="md" bg="dark" variant="dark" fixed="top">
-      <Navbar.Brand href="/">OCAT</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/">Dashboard</Nav.Link>
-        <Nav.Link href="/assessment/new">New Assessment</Nav.Link>
-      </Nav>
+      <Container>
+        <Navbar.Brand href="/">OCAT</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Dashboard</Nav.Link>
+            <Nav.Link href="/assessment/new">New Assessment</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   </header>;
