43aaefd0dafc3c0b6954ba833d0cfc206446d0bd
Wes Reed <reedws@ucmail.uc.edu>
2022-12-15 12:35:36 -0500

    fix browser router

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { SiteWrapper } from './components';
 import { DashboardBulletin } from './pages/Dashboard/DashboardBulletin';
 import { NewAssessment } from './pages/Assessments/NewAssessment.jsx';
 import { AssessmentList } from './pages/Assessments/AssessmentList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => <SiteWrapper>
-  <BrowserRouter>
-    <Route path="/" component={DashboardBulletin} />
-    <Route path="/assessment/new" component={NewAssessment} />
+const router = createBrowserRouter([
+  {
+    element: <DashboardBulletin />,
+    path: `/`,
+  },
+  {
+    element: <NewAssessment />,
+    path: `/assessment/new`,
+  },
+]);
 
-  </BrowserRouter>
+const App = () => <SiteWrapper>
+  <RouterProvider router={router} />
 </SiteWrapper>;
 
 export default App;
